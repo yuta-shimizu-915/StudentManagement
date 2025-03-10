09bf41e3331b0e44925a55e42a0638dc0076d34c
shimizu <alfa.u915@gmail.com>
2025-03-10 11:25:15 +0900

    課題10に変更したコミット

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,20 +1,32 @@
 package raisetech.StudentManagement;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	@Autowired
+	private StudentRepository repository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/good morning")
-	public String good_morning() {
-		return "good morning! everyone!";
+	@GetMapping("/studentList")
+	public List<Student> getStudentList() {
+
+		return repository.search();
+	}
+
+	@GetMapping("/studentscourses")
+	public List<Studentcourses> getStudentCoursesList(){
+		return repository.search_co();
 	}
 }
---
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -0,0 +1,15 @@
+package raisetech.StudentManagement;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface StudentRepository {
+
+  @Select("SELECT * FROM students")
+  List<Student> search();
+
+  @Select("SELECT * FROM students_courses")
+  List<Studentcourses> search_co();
+}
