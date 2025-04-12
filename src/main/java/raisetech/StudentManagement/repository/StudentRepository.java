e588234b473b9c317536da051a1ba9bd5732552b
shimizu <alfa.u915@gmail.com>
2025-04-12 12:24:20 +0900

    課題16に変更したコミット

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -3,7 +3,10 @@ package raisetech.StudentManagement.repository;
 import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourses;
 
@@ -18,7 +21,28 @@ public interface StudentRepository {
   List<StudentCourses> searchCourse();
 
   @Insert(
-      "INSERT INTO students (student_id,name,furigana,nickname,e_mail,region,age,gender,remark,isDeletes) "
-          + "VALUES(#{studentId},#{name},#{furigana},#{nickName},#{email},#{region},#{age},#{gender},#{remark},false)")
+      "INSERT INTO students (name,furigana,nickname,e_mail,region,age,gender,remark,isDeletes) "
+          + "VALUES(#{name},#{furigana},#{nickName},#{email},#{region},#{age},#{gender},#{remark},false)")
+  @Options(useGeneratedKeys = true, keyProperty = "studentId")
   void registerStudent(Student student);
+
+  @Insert("INSERT INTO students_courses(student_id,course_name,start_date,finish_date)"
+      + "VALUE(#{studentId},#{courseName},#{startDate},#{finishDate})")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void registerStudentsCourses(StudentCourses studentCourses);
+
+  @Update("UPDATE students SET name=#{name},furigana=#{furigana}, nickname=#{nickName}, e_mail=#{email}, region=#{region}, age=#{age}, gender=#{gender}, remark=#{remark} WHERE student_id=#{studentId}")
+  void updateStudent(Student student);
+
+  @Update("UPDATE students_courses SET course_name=#{courseName}, start_date=#{startDate}, finish_date=#{finishDate} WHERE id=#{id}")
+  void updateStudentCourse(StudentCourses studentCourses);
+
+  @Select("SELECT student_id, name, furigana, nickname, e_mail, region, age, gender, remark  "
+      + "FROM students WHERE student_id = #{id}")
+  Student findStudentById(@Param("id") String id);
+
+  @Select("SELECT student_id, course_name, start_date, finish_date " +
+      "FROM students_courses WHERE student_id = #{id}")
+  List<StudentCourses> findStudentCoursesById(@Param("id") String id);
+
 }
